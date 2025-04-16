b1033f40c47d059e7edd5dd2796a35fdf08c93f6
joearias1121 <joearias1121@gmail.com>
2025-04-15 23:51:00 -0400

    imported axios and using it to interact with backend for submitting a note

--- src/app/notes/page.tsx
+++ src/app/notes/page.tsx
@@ -1,22 +1,45 @@
 'use client'
 import { useState } from 'react';
+import axios from 'axios';
 
 function NotesPage() {
   const [content, setContent] = useState('');
   const [title, setTitle] = useState('');
 
-  const onSubmit = (formData: FormData) => {
-    console.log(formData);
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault()
+    try {
+      const response = await axios.post('/api/notes', {
+        title,
+        content,
+        userId: 1,
+      })
+      console.log("Success:", response.data);
+    }catch (err) {
+      console.error('Error creating note', err)
+      alert('Error creating note')
+    }
 
   }
 
   return (
     <>
       <h1>Notes</h1>
-      <form action="onSubmit" method="POST">
+      <form onSubmit={handleSubmit} method="POST">
         <input type="number" name="userId" placeholder="User ID" />
-        <input type="text" value={title} name="title" onChange={e => setTitle(e.target.value)} placeholder="Title" />
-        <textarea name="content" value={content} placeholder="Type..."></textarea>
+        <input
+          type="text"
+          value={title}
+          name="title"
+          onChange={(e) => setTitle(e.target.value)}
+          placeholder="Title"
+        />
+        <textarea
+          name="content"
+          value={content}
+          onChange={(e) => setTitle(e.target.value)}
+          placeholder="Type..."
+        ></textarea>
         <button type="submit">Submit</button>
       </form>
     </>
