500ff496d2e7df78fa777e20afb2092e176198bd
joearias1121 <joearias1121@gmail.com>
2025-11-30 23:57:54 -0500

    adding shadcn and header

---
+++ components/Header.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Button } from '@/components/ui/button';
+
+function Header(props) {
+  return (
+    <div>
+      <div>
+        Notaibly
+      </div>
+      <Button>
+        
+      </Button>
+    </div>
+  );
+}
+
+export default Header;
